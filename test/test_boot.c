e3f600cab0098c6163c5e82ddc60b200db58e1bd
brendon <brendon.lecomte@gmail.com>
2018-02-02 17:13:11 +0000

    add final boot comments

--- test/test_boot.c
+++ test/test_boot.c
@@ -58,3 +58,41 @@ TEST(Boot, audio) {
     TEST_ASSERT_EQUAL(0xf3, memory->memory[0xff12]); //check we cleared it which
     TEST_ASSERT_EQUAL(0x77, memory->memory[0xff24]); //check we cleared it which
 }
+
+
+// AF=$01B0
+// BC=$0013
+// DE=$00D8
+// HL=$014D
+// Stack Pointer=$FFFE
+// [$FF05] = $00   ; TIMA
+// [$FF06] = $00   ; TMA
+// [$FF07] = $00   ; TAC
+// [$FF10] = $80   ; NR10
+// [$FF11] = $BF   ; NR11
+// [$FF12] = $F3   ; NR12
+// [$FF14] = $BF   ; NR14
+// [$FF16] = $3F   ; NR21
+// [$FF17] = $00   ; NR22
+// [$FF19] = $BF   ; NR24
+// [$FF1A] = $7F   ; NR30
+// [$FF1B] = $FF   ; NR31
+// [$FF1C] = $9F   ; NR32
+// [$FF1E] = $BF   ; NR33
+// [$FF20] = $FF   ; NR41
+// [$FF21] = $00   ; NR42
+// [$FF22] = $00   ; NR43
+// [$FF23] = $BF   ; NR30
+// [$FF24] = $77   ; NR50
+// [$FF25] = $F3   ; NR51
+// [$FF26] = $F1-GB, $F0-SGB ; NR52
+// [$FF40] = $91   ; LCDC
+// [$FF42] = $00   ; SCY
+// [$FF43] = $00   ; SCX
+// [$FF45] = $00   ; LYC
+// [$FF47] = $FC   ; BGP
+// [$FF48] = $FF   ; OBP0
+// [$FF49] = $FF   ; OBP1
+// [$FF4A] = $00   ; WY
+// [$FF4B] = $00   ; WX
+// [$FFFF] = $00   ; IE
