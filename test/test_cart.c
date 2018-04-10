cc71879fb153acf22a957a160234ab8244256e4e
brendon <brendon.lecomte@gmail.com>
2018-04-10 18:11:46 +0100

    remove old memory artifacts replace memory address with cart function

--- test/test_cart.c
+++ test/test_cart.c
@@ -14,17 +14,13 @@ TEST_SETUP(Cart) {
 TEST_TEAR_DOWN(Cart) {}
 
 TEST(Cart, load) {
-    uint8_t mem[256];
-    mem[0] = 0;
     cart_load();
     TEST_ASSERT_EQUAL(0xC3, cart_read(0));
 }
 
 TEST(Cart, header) {
-  uint8_t mem[0x8000];
-  mem[0] = 0;
   cart_load();
-  cart_t *cart = (cart_t *)&mem[0x100];
+  cart_t *cart = (cart_t *)cart_memory();
   TEST_ASSERT_EQUAL_STRING("TETRIS", cart->title);
   TEST_ASSERT_EQUAL(0x0A, cart->header_checksum);
   TEST_ASSERT_EQUAL(0xBF16, cart->checksum);
