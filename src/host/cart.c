c92350518c525718723d6cb2f337ec7616ac4bf9
brendon <brendon.lecomte@gmail.com>
2018-04-16 17:50:33 +0100

    define all the individual CPU instr tests

--- src/host/cart.c
+++ src/host/cart.c
@@ -2,7 +2,18 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define CART_PATH ("/Users/brendon/research/gb/roms/cpu_instrs.gb")
+#define TEST_ALL_PATH ("/Users/brendon/research/gb/roms/cpu_instrs.gb")
+#define TEST_01_PATH ("/Users/brendon/research/gb/roms/individual/01-special.gb")
+#define TEST_02_PATH ("/Users/brendon/research/gb/roms/individual/02-interrupts.gb")
+#define TEST_03_PATH ("/Users/brendon/research/gb/roms/individual/03-op sp,hl.gb")
+#define TEST_04_PATH ("/Users/brendon/research/gb/roms/individual/04-op r,imm.gb")
+#define TEST_05_PATH ("/Users/brendon/research/gb/roms/individual/05-op rp.gb")
+#define TEST_06_PATH ("/Users/brendon/research/gb/roms/individual/06-ld r,r.gb")
+#define TEST_07_PATH ("/Users/brendon/research/gb/roms/individual/07-jr,jp,call,ret,rst.gb")
+#define TEST_08_PATH ("/Users/brendon/research/gb/roms/individual/08-misc instrs.gb")
+#define TEST_09_PATH ("/Users/brendon/research/gb/roms/individual/09-op r,r.gb")
+#define TEST_10_PATH ("/Users/brendon/research/gb/roms/individual/10-bit ops.gb")
+#define TEST_11_PATH ("/Users/brendon/research/gb/roms/individual/11-op a,(hl).gb")
 #define HOME_CART_PATH ("/Users/brendon/research/gb/roms/tetris.gb")
 
 #define MAX_CART_SIZE (0x8000)
@@ -11,7 +22,7 @@ static uint8_t cart[MAX_CART_SIZE];
 
 void cart_load(void) {
     FILE *f;
-    f = fopen(CART_PATH, "rb");
+    f = fopen(TEST_ALL_PATH, "rb");
     if(f){
         fread(cart, 1, MAX_CART_SIZE, f);
         fclose(f);
