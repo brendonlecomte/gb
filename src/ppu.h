0273747745713b98cd2d4acc80f9a9b554d94fb1
brendon <brendon.lecomte@gmail.com>
2018-04-23 16:59:46 +0100

    add sprite enable flag to LCDC reformat structs for readability

--- src/ppu.h
+++ src/ppu.h
@@ -5,11 +5,24 @@
 #include <stdbool.h>
 
 typedef struct {
-  uint8_t enable:1,tile_map_select:1,window_enable:1, bg_win_tile_select:1, bg_tile_select:1, sprite_size:1, bg_win_priority:1;
+  uint8_t enable:1,
+  tile_map_select:1,
+  window_enable:1,
+  bg_win_tile_select:1,
+  bg_tile_select:1,
+  sprite_size:1,
+  sprite_enable:1,
+  bg_win_priority:1;
 } lcd_control_t;
 
 typedef struct {
-  uint8_t mode:2, coincidence_flag:1, h_blank_int:1, v_blank_int:1, oam_int:1, coincidence_int:1, unused:1;
+  uint8_t mode:2,
+  coincidence_flag:1,
+  h_blank_int:1,
+  v_blank_int:1,
+  oam_int:1, 
+  coincidence_int:1,
+  unused:1;
 } lcd_status_register_t;
 
 typedef struct {
