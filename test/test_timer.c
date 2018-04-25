28bafe6e22c9e8264f2866bda44ea1d2db208a78
brendon <brendon.lecomte@gmail.com>
2018-04-25 17:05:16 +0100

    fix timer test doing memory exceptions

--- test/test_timer.c
+++ test/test_timer.c
@@ -26,39 +26,39 @@ TEST(Timer, run_len)
 {
 
     t.div = 0;
-    t.counter = 0;
+    *t.counter = 0;
     t.prev_div = 0;
-    t.control = TIMER_ENABLE | TIMER_DIV_16;
+    *t.control = TIMER_ENABLE | TIMER_DIV_16;
     for(uint16_t i = 0; i < 16; i++) timer_run(&t);
     TEST_ASSERT_EQUAL(1, t.counter);
 
     t.div = 0;
-    t.counter = 0;
+    *t.counter = 0;
     t.prev_div = 0;
-    t.control = TIMER_ENABLE | TIMER_DIV_1024;
+    *t.control = TIMER_ENABLE | TIMER_DIV_1024;
     for(uint16_t i = 0; i < 1024; i++) timer_run(&t);
     TEST_ASSERT_EQUAL(1, t.counter);
 
     t.div = 0;
     t.prev_div = 0;
-    t.counter = 0;
-    t.control = TIMER_ENABLE | TIMER_DIV_64;
+    *t.counter = 0;
+    *t.control = TIMER_ENABLE | TIMER_DIV_64;
     for(uint16_t i = 0; i < 64; i++) timer_run(&t);
     TEST_ASSERT_EQUAL(1, t.counter);
 
     t.div = 0;
     t.prev_div = 0;
-    t.counter = 0;
-    t.control = TIMER_ENABLE | TIMER_DIV_256;
+    *t.counter = 0;
+    *t.control = TIMER_ENABLE | TIMER_DIV_256;
     for(uint16_t i = 0; i < 256; i++) timer_run(&t);
     TEST_ASSERT_EQUAL(1, t.counter);
 }
 
 TEST(Timer, reload)
 {
-    t.counter = 0xFE;
-    t.modulo = 0xAA;
-    t.control = TIMER_ENABLE | TIMER_DIV_16;
+    *t.counter = 0xFE;
+    *t.modulo = 0xAA;
+    *t.control = TIMER_ENABLE | TIMER_DIV_16;
     t.div = 0x10;
     timer_run(&t);
     TEST_ASSERT_EQUAL(0xFF, t.counter);
@@ -70,11 +70,11 @@ TEST(Timer, reload)
 
 TEST(Timer, enable)
 {
-    t.control = TIMER_DIV_16;
+    *t.control = TIMER_DIV_16;
     for(uint16_t i = 0; i < 32; i++) timer_run(&t);
     TEST_ASSERT_EQUAL(0, t.counter);
 
-    t.control |= TIMER_ENABLE;
+    *t.control |= TIMER_ENABLE;
     for(uint16_t i = 0; i < 16; i++) timer_run(&t);
     TEST_ASSERT_EQUAL(1, t.counter);
 
