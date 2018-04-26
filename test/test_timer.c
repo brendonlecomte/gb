eff2fadd286fe8c627a750a5e93f9766be0158fc
brendon <brendon.lecomte@gmail.com>
2018-04-26 16:43:37 +0100

    timer test was using address of, not value of, counter for comaprison

--- test/test_timer.c
+++ test/test_timer.c
@@ -30,28 +30,28 @@ TEST(Timer, run_len)
     t.prev_div = 0;
     *t.control = TIMER_ENABLE | TIMER_DIV_16;
     for(uint16_t i = 0; i < 16; i++) timer_run(&t);
-    TEST_ASSERT_EQUAL(1, t.counter);
+    TEST_ASSERT_EQUAL(1, *t.counter);
 
     t.div = 0;
     *t.counter = 0;
     t.prev_div = 0;
     *t.control = TIMER_ENABLE | TIMER_DIV_1024;
     for(uint16_t i = 0; i < 1024; i++) timer_run(&t);
-    TEST_ASSERT_EQUAL(1, t.counter);
+    TEST_ASSERT_EQUAL(1, *t.counter);
 
     t.div = 0;
     t.prev_div = 0;
     *t.counter = 0;
     *t.control = TIMER_ENABLE | TIMER_DIV_64;
     for(uint16_t i = 0; i < 64; i++) timer_run(&t);
-    TEST_ASSERT_EQUAL(1, t.counter);
+    TEST_ASSERT_EQUAL(1, *t.counter);
 
     t.div = 0;
     t.prev_div = 0;
     *t.counter = 0;
     *t.control = TIMER_ENABLE | TIMER_DIV_256;
     for(uint16_t i = 0; i < 256; i++) timer_run(&t);
-    TEST_ASSERT_EQUAL(1, t.counter);
+    TEST_ASSERT_EQUAL(1, *t.counter);
 }
 
 TEST(Timer, reload)
@@ -61,21 +61,21 @@ TEST(Timer, reload)
     *t.control = TIMER_ENABLE | TIMER_DIV_16;
     t.div = 0x10;
     timer_run(&t);
-    TEST_ASSERT_EQUAL(0xFF, t.counter);
+    TEST_ASSERT_EQUAL(0xFF, *t.counter);
 
     t.div = 0xFF;
     timer_run(&t);
-    TEST_ASSERT_EQUAL(0xAA, t.counter);
+    TEST_ASSERT_EQUAL(0xAA, *t.counter);
 }
 
 TEST(Timer, enable)
 {
     *t.control = TIMER_DIV_16;
     for(uint16_t i = 0; i < 32; i++) timer_run(&t);
-    TEST_ASSERT_EQUAL(0, t.counter);
+    TEST_ASSERT_EQUAL(0, *t.counter);
 
     *t.control |= TIMER_ENABLE;
     for(uint16_t i = 0; i < 16; i++) timer_run(&t);
-    TEST_ASSERT_EQUAL(1, t.counter);
+    TEST_ASSERT_EQUAL(1, *t.counter);
 
 }
