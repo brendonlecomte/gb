3705eae7185a80a6bd40c4a3402424f3c9c622f7
brendon <brendon.lecomte@gmail.com>
2018-07-09 16:45:42 +0100

    update test_timer to use new gb_timer

--- test/test_timer.c
+++ test/test_timer.c
@@ -1,14 +1,14 @@
-#include "timer.h"
+#include "gb_timer.h"
 #include "unity.h"
 #include "unity_fixture.h"
 
 TEST_GROUP(Timer);
 
-static timer_t t;
+static gb_timer_t t;
 
 TEST_SETUP(Timer)
 {
-    timer_init(&t);
+    gb_timer_init(&t);
 }
 
 TEST_TEAR_DOWN(Timer)
@@ -18,7 +18,7 @@ TEST_TEAR_DOWN(Timer)
 TEST(Timer, init)
 {
     t.div =44;
-    timer_init(&t);
+    gb_timer_init(&t);
     TEST_ASSERT_EQUAL(t.div, 0);
 }
 
@@ -28,29 +28,29 @@ TEST(Timer, run_len)
     t.div = 0;
     *t.counter = 0;
     t.prev_div = 0;
-    *t.control = TIMER_ENABLE | TIMER_DIV_16;
-    for(uint16_t i = 0; i < 16; i++) timer_run(&t);
+    *t.control = GB_TIMER_ENABLE | GB_TIMER_DIV_16;
+    for(uint16_t i = 0; i < 16; i++) gb_timer_run(&t);
     TEST_ASSERT_EQUAL(1, *t.counter);
 
     t.div = 0;
     *t.counter = 0;
     t.prev_div = 0;
-    *t.control = TIMER_ENABLE | TIMER_DIV_1024;
-    for(uint16_t i = 0; i < 1024; i++) timer_run(&t);
+    *t.control = GB_TIMER_ENABLE | GB_TIMER_DIV_1024;
+    for(uint16_t i = 0; i < 1024; i++) gb_timer_run(&t);
     TEST_ASSERT_EQUAL(1, *t.counter);
 
     t.div = 0;
     t.prev_div = 0;
     *t.counter = 0;
-    *t.control = TIMER_ENABLE | TIMER_DIV_64;
-    for(uint16_t i = 0; i < 64; i++) timer_run(&t);
+    *t.control = GB_TIMER_ENABLE | GB_TIMER_DIV_64;
+    for(uint16_t i = 0; i < 64; i++) gb_timer_run(&t);
     TEST_ASSERT_EQUAL(1, *t.counter);
 
     t.div = 0;
     t.prev_div = 0;
     *t.counter = 0;
-    *t.control = TIMER_ENABLE | TIMER_DIV_256;
-    for(uint16_t i = 0; i < 256; i++) timer_run(&t);
+    *t.control = GB_TIMER_ENABLE | GB_TIMER_DIV_256;
+    for(uint16_t i = 0; i < 256; i++) gb_timer_run(&t);
     TEST_ASSERT_EQUAL(1, *t.counter);
 }
 
@@ -58,24 +58,24 @@ TEST(Timer, reload)
 {
     *t.counter = 0xFE;
     *t.modulo = 0xAA;
-    *t.control = TIMER_ENABLE | TIMER_DIV_16;
+    *t.control = GB_TIMER_ENABLE | GB_TIMER_DIV_16;
     t.div = 0x10;
-    timer_run(&t);
+    gb_timer_run(&t);
     TEST_ASSERT_EQUAL(0xFF, *t.counter);
 
     t.div = 0xFF;
-    timer_run(&t);
+    gb_timer_run(&t);
     TEST_ASSERT_EQUAL(0xAA, *t.counter);
 }
 
 TEST(Timer, enable)
 {
-    *t.control = TIMER_DIV_16;
-    for(uint16_t i = 0; i < 32; i++) timer_run(&t);
+    *t.control = GB_TIMER_DIV_16;
+    for(uint16_t i = 0; i < 32; i++) gb_timer_run(&t);
     TEST_ASSERT_EQUAL(0, *t.counter);
 
-    *t.control |= TIMER_ENABLE;
-    for(uint16_t i = 0; i < 16; i++) timer_run(&t);
+    *t.control |= GB_TIMER_ENABLE;
+    for(uint16_t i = 0; i < 16; i++) gb_timer_run(&t);
     TEST_ASSERT_EQUAL(1, *t.counter);
 
 }
