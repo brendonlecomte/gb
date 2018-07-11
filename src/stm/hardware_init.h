c068d5acc26420b62265e10357674052f2bd716f
brendon <brendon.lecomte@gmail.com>
2018-07-11 16:25:50 +0100

    create new stm main. and hardware init
    makefile_target now has gen source and include seperate so that adding
    files is easoer

---
+++ src/stm/hardware_init.h
@@ -0,0 +1,8 @@
+#ifndef HARDWARE_INIT_H_
+#define HARDWARE_INIT_H_
+
+
+void hardware_init(void);
+
+
+#endif
---
+++ src/stm/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "hardware_init.h"
+
+
+int main(void)
+{
+  hardware_init();
+
+  while (1)
+  {
+
+  }
+}
