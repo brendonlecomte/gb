d9be87fe08d42cb861cf6cd4d693b42e41155080
brendon <brendon.lecomte@gmail.com>
2018-07-12 16:27:25 +0100

    remove cart directory defines

--- src/host/cart.c
+++ src/host/cart.c
@@ -2,25 +2,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-
-#define TEST_ALL_PATH ("/Users/brendon/research/gb/roms/cpu_instrs.gb")
-#define TEST_OPUS_PATH ("/Users/brendon/research/gb/roms/opus5.gb")
-#define TEST_01_PATH ("/Users/brendon/research/gb/roms/individual/01-special.gb")
-#define TEST_02_PATH ("/Users/brendon/research/gb/roms/individual/02-interrupts.gb")
-#define TEST_03_PATH ("/Users/brendon/research/gb/roms/individual/03-op sp,hl.gb")
-#define TEST_04_PATH ("/Users/brendon/research/gb/roms/individual/04-op r,imm.gb")
-#define TEST_05_PATH ("/Users/brendon/research/gb/roms/individual/05-op rp.gb")
-#define TEST_06_PATH ("/Users/brendon/research/gb/roms/individual/06-ld r,r.gb")
-#define TEST_07_PATH ("/Users/brendon/research/gb/roms/individual/07-jr,jp,call,ret,rst.gb")
-#define TEST_08_PATH ("/Users/brendon/research/gb/roms/individual/08-misc instrs.gb")
-#define TEST_09_PATH ("/Users/brendon/research/gb/roms/individual/09-op r,r.gb")
-#define TEST_10_PATH ("/Users/brendon/research/gb/roms/individual/10-bit ops.gb")
-#define TEST_11_PATH ("/Users/brendon/research/gb/roms/individual/11-op a,(hl).gb")
-#define HOME_CART_PATH ("/Users/brendon/research/gb/roms/tetris.gb")
-
-
 extern uint8_t* game_cart;
-
 
 void cart_load(void) {
   // does nothing handled by script...
