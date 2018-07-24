9e45a80618c428babfb4b0ce3dbaf6ffa0763359
brendon <brendon.lecomte@gmail.com>
2018-07-24 14:09:40 +0100

    memory cannot be tested in the cart range.

--- test/test_memory.c
+++ test/test_memory.c
@@ -19,33 +19,33 @@ TEST_TEAR_DOWN(Memory)
 
 TEST(Memory, init)
 {
-    TEST_ASSERT_EQUAL(0x31, test_mem.cart[0]);
-    TEST_ASSERT_EQUAL(0x21, test_mem.cart[4]);
+    TEST_ASSERT_EQUAL(0x0, test_mem.cart[0]);
+    TEST_ASSERT_EQUAL(0x0, test_mem.cart[4]);
 }
 
 TEST(Memory, read8)
 {
-    memory_write8(&test_mem, 7, 0xCC);
-    TEST_ASSERT_EQUAL(0xCC, memory_read8(&test_mem, 7));
+    memory_write8(&test_mem, 0x8107, 0xCC);
+    TEST_ASSERT_EQUAL(0xCC, memory_read8(&test_mem, 0x8107));
 }
 
 TEST(Memory, read16)
 {
-    memory_write16(&test_mem, 2, 0x55AA);
-    TEST_ASSERT_EQUAL(0x55AA, memory_read16(&test_mem, 2));
+    memory_write16(&test_mem, 0x8521, 0x55AA);
+    TEST_ASSERT_EQUAL(0x55AA, memory_read16(&test_mem, 0x8521));
 }
 
 
 TEST(Memory, write8)
 {
-    memory_write8(&test_mem, 0x1234, 0xCC);
-    TEST_ASSERT_EQUAL(0xCC, memory_read8(&test_mem, 0x1234));
+    memory_write8(&test_mem, 0x9234, 0xCC);
+    TEST_ASSERT_EQUAL(0xCC, memory_read8(&test_mem, 0x9234));
 }
 
 TEST(Memory, write16)
 {
-    memory_write16(&test_mem, 0x892, 0x55AA);
-    TEST_ASSERT_EQUAL(0x55AA, memory_read16(&test_mem, 0x892));
+    memory_write16(&test_mem, 0xA892, 0x55AA);
+    TEST_ASSERT_EQUAL(0x55AA, memory_read16(&test_mem, 0xA892));
 }
 
 TEST(Memory, inboot)
